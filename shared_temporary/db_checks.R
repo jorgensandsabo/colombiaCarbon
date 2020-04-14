2d1e14cd8eba63563aff759d9c1d47b8d252a290
jorgensandsabo <48399478+jorgensandsabo@users.noreply.github.com>
2020-04-14 17:00:41 +0200

    Update db_checks.R

--- shared_temporary/db_checks.R
+++ shared_temporary/db_checks.R
@@ -9,6 +9,8 @@ jorgen.desktop <- file.exists('C:\\Users\\Jorgen\\OneDrive - Norwegian Universit
 if(jorgen.desktop){
   dir.path <- "C:\\Users\\Jorgen\\OneDrive - Norwegian University of Life Sciences\\PhD"}
 
+setwd(dir.path)
+
 # Read file with cluster names
 jorg <- read.csv(paste(dir.path,"\\Data\\raw\\Plots_BM.csv",sep=""))
 names(jorg)[which(names(jorg)=="SiteCode")] <- "Cod_Loc"
@@ -52,7 +54,7 @@ dbtab <- table(db$Cod_Loc,db$Day,db$Cientific.Name)
     ntrapdays <- length(dbtab[1,,1])
     hab <- as.numeric(as.factor(as.data.frame(db %>% group_by(db$Cod_Loc) %>% summarise(hab=first(Habitat)))$hab))
     specname <- sort(unique(db$Cientific.Name))#;specname <- specname[-1]
-
+    
 for(i in 1:nspecies){
   for(j in 1:nsites){
     for(k in 1:ntrapdays){
@@ -62,7 +64,6 @@ for(i in 1:nspecies){
                                   & db$Day == sort(unique(db$Day))[k])
                             ,]$Abundance
       }}}}    
-
 
 
 # Proportion of captures per trap-day for each species and trap
@@ -113,6 +114,128 @@ DHARMa::testZeroInflation(modfit_res_overdisp)
 DHARMa::plotSimulatedResiduals(modfit_res_overdisp)
 
 
+###### Abundance model, no detection
+model1.string <- "
+  model {
+
+  # Process model
+    for(i in 1:nspecies){
+      for(j in 1:nsites){
+        for(k in 1:ntrapdays){
+          
+          count[j,k,i] ~ dpois(lambda[j,k,i]*ORLE[j,k,i])
+          log(lambda[j,k,i]) <- alpha[i] + beta1[i,hab[j]] + beta2[i,hab[j]] * rebait[k]
+
+        }
+      }
+    }
+
+  # Priors
+  mua ~ dnorm(0,1)
+  sda ~ dunif(0,10)
+  taua <- pow(sda,-2)
+  
+  mub1 ~ dnorm(0,1)
+  sdb1 ~ dunif(0,10)
+  taub1 <- pow(sdb1,-2)
+  
+  mub2 ~ dnorm(0,1)
+  sdb2 ~ dunif(0,10)
+  taub2 <- pow(sdb2,-2)
+  
+  for(i in 1:nspecies){
+    alpha[i] ~ dnorm(mua,taua)
+    
+    for(h in 1:2){
+      beta1[i,h] ~ dnorm(mub1,taub1)
+      beta2[i,h] ~ dnorm(mub2,taub2)
+    }
+  }
+  
+  # Priors for overdispersion term
+ # tauORLE ~ dgamma(1,1)
+
+  for(i in 1:nspecies){
+    for(j in 1:nsites){
+      for(k in 1:ntrapdays){
+          ORLE[j,k,i] ~ dgamma(1,1)
+      }
+    }
+  }
+}"
+
+sink("JAGS\\abunmod.txt")
+cat(paste(model1.string))
+sink()
+
+DBmod_abunmod <- rjags::jags.model(file = "JAGS\\abunmod.txt",
+                                   data=list(count = dbtab, hab=hab, rebait=c(0,1,0,1), nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
+                                   #inits = list(abun = apply(dbtab,c(1,3),sum)),
+                                   n.chains=3,n.adapt=1000)
+update(object = DBmod_abunmod, n.iter = 1000)
+DBmod_abunmod <- rjags::coda.samples(model = DBmod_abunmod,
+                                     variable.names = c("lambda","mua","sda","mub1","sdb1","mub2","sdb2","alpha","beta1","beta2"),
+                                     n.iter=1000, thin = 1)
+
+### Model checks
+MCMCvis::MCMCplot(DBmod_abunmod)
+MCMCvis::MCMCtrace(DBmod_abunmod,wd="C:\\Users\\Jorgen\\Desktop",filename="DBmod_abunmod.pdf")
+MCMCvis::MCMCsummary(DBmod_abunmod)
+
+
+###### Removal model
+model1.string <- "
+  model {
+
+  # Process model
+    for(i in 1:nspecies){
+      for(j in 1:nsites){
+        for(k in 1:ntrapdays){
+        
+          abun[j,k+1,i] <- abun[j,k,i] - count[j,k,i]
+          count[j,k,i] ~ dbin(q[i,hab[j],rebait[k]],abun[j,k,i])
+          #logit(q[i,j,k]) <- alpha[i,hab[j]] + beta[i,hab[j]] * rebait[k]
+        }
+        abun[j,1,i] ~  dpois(u[j,i])
+      }
+    }
+
+  # Priors
+  for(i in 1:nspecies){
+
+    for(h in 1:2){
+      #alpha[i,h] ~ dnorm(0,1)
+      #beta[i,h] ~ dnorm(0,1)
+      
+      for(r in 1:2){
+        q[i,h,r] ~ dbeta(3,3)
+      }
+    }
+
+    for(j in 1:nsites){
+      u[j,i] ~ dgamma(1,0.001)
+    }
+  }
+}"
+
+sink("JAGS\\removalmod.txt")
+cat(paste(model1.string))
+sink()
+
+DBmod_removal <- rjags::jags.model(file = "JAGS\\removalmod.txt",
+                                 data=list(count = dbtab, hab=hab, rebait=c(1,2,1,2), nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
+                                 #inits = list(abun = apply(dbtab,c(1,3),sum)),
+                                 n.chains=3,n.adapt=1000)
+update(object = DBmod_removal, n.iter = 1000)
+DBmod_removal <- rjags::coda.samples(model = DBmod_removal,
+                                   variable.names = c("u","beta","alpha","q","abun"),
+                                   n.iter=1000, thin = 1)
+
+### Model checks
+MCMCvis::MCMCplot(DBmod_removal)
+MCMCvis::MCMCtrace(DBmod_removal,wd="C:\\Users\\Jorgen\\Desktop",filename="DBmod_removal.pdf")
+
+
 
 ###### Poisson abundance model - no shared species parameter distribution
 model1.string <- "
@@ -127,15 +250,20 @@ model1.string <- "
 
         # Observation process
         for(k in 1:ntrapdays){
-          count[j,k,i] ~ dbin(d[i,hab[j]],abun[j,i])
+          count[j,k,i] ~ dbin(d[i,j,k],abun[j,i])
+          logit(d[i,j,k]) <- alpha[i,hab[j]] + betad[i,k] * rebait[k]
         }
       }
     }
 
   # Priors
   for(i in 1:nspecies){
+    for(k in 1:ntrapdays){
+      betad[i,k] ~ dnorm(0,1)
+    }
     for(m in 1:2){
-        d[i,m] ~ dbeta(1,1)
+        #d[i,m] ~ dbeta(1,1)
+        alpha[i,m] ~ dnorm(0,1)
         lambda[i,m] ~ dgamma(1,0.001)
     }
   }
@@ -146,7 +274,7 @@ cat(paste(model1.string))
 sink()
 
 DBmod_Pois1 <- rjags::jags.model(file = "JAGS\\Nmod_poisson1.txt",
-                                data=list(count = dbtab, hab=hab, nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
+                                data=list(count = dbtab, hab=hab, rebait=c(1,2,1,2), nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
                                 inits =list(abun = apply(dbtab,c(1,3),sum)),
                                 n.chains=3,n.adapt=1000)
 update(object = DBmod_Pois1, n.iter = 1000)
@@ -155,8 +283,8 @@ DBmod_Pois1 <- rjags::coda.samples(model = DBmod_Pois1,
                                   n.iter=1000, thin = 1)
 
 ### Model checks
-MCMCvis::MCMCplot()
-MCMCvis::MCMCtrace(DBmod_OD,wd="C:\\Users\\Jorgen\\Desktop",filename="DBmod_Pois1.pdf")
+MCMCvis::MCMCplot(DBmod_Pois1)
+MCMCvis::MCMCtrace(DBmod_Pois1,wd="C:\\Users\\Jorgen\\Desktop",filename="DBmod_Pois1.pdf")
 
 ###### Poisson abundance model - shared species parameter distribution
 model1.string <- "
@@ -171,7 +299,8 @@ model1.string <- "
         
         # Observation process
         for(k in 1:ntrapdays){
-          count[j,k,i] ~ dbin(d[i,hab[j]],abun[j,i])
+          count[j,k,i] ~ dbin(d[i,j,k],abun[j,i])
+          logit(d[i,j,k]) <- alpha[i,hab[j]] + betad[i,k] * rebait[k]
         }
       }
     }
@@ -185,9 +314,13 @@ model1.string <- "
   taul <- pow(sdl,-2)
 
   for(i in 1:nspecies){
+    for(k in 1:ntrapdays){
+      betad[i,k] ~ dnorm(0,1)
+    }
     for(m in 1:2){
-        a[i,m] ~ dnorm(meand,taud)
-        logit(d[i,m]) <- a[i,m]
+        alpha[i,m] ~ dnorm(meand,taud)
+        #a[i,m] ~ dnorm(meand,taud)
+        #logit(d[i,m]) <- a[i,m]
         b[i,m] ~ dnorm(meanl,taul)
         log(lambda[i, m]) <- b[i,m]
     }
@@ -199,13 +332,18 @@ cat(paste(model1.string))
 sink()
 
 DBmod_Pois <- rjags::jags.model(file = "JAGS\\Nmod_poisson.txt",
-                              data=list(count = dbtab, hab=hab, nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
+                              data=list(count = dbtab, hab=hab,rebait=c(1,2,1,2), nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
                               inits =list(abun = apply(dbtab,c(1,3),sum)),
-                              n.chains=3,n.adapt=1000)
-update(object = DBmod_Pois, n.iter = 1000)
+                              n.chains=3,n.adapt=10000)
+update(object = DBmod_Pois, n.iter = 10000)
 DBmod_Pois <- rjags::coda.samples(model = DBmod_Pois,
                                 variable.names = c("d","lambda","meand","sdd","meanl","sdl","abun"),
-                                n.iter=1000, thin = 1)
+                                n.iter=10000, thin = 1)
+
+### Model checks
+MCMCvis::MCMCplot(DBmod_Pois)
+MCMCvis::MCMCtrace(DBmod_Pois,wd="C:\\Users\\Jorgen\\Desktop",filename="DBmod_Pois1.pdf")
+
 
 
 
@@ -220,8 +358,9 @@ model1.string <- "
       for(j in 1:nsites){
         # Observation process
         for(k in 1:ntrapdays){
-          abun[j,k,i] ~ dpois(lambda[i,hab[j]] * exp(ORLE[j,k,i]))
-          count[j,k,i] ~ dbin(d[i,hab[j]],abun[j,k,i])
+          abun[j,k,i] ~ dpois(lambda[i,hab[j]] * ORLE[j,k,i])
+          count[j,k,i] ~ dbin(d[i,j,k],abun[j,k,i])
+          logit(d[i,j,k]) <- alpha[i,hab[j]] + betad[i,k] * rebait[k]
         }
       }
     }
@@ -229,14 +368,19 @@ model1.string <- "
   # Priors
   meand ~ dnorm(0,1)
   sdd ~ dgamma(1,0.001)
+  taud <- pow(sdd,-2)
   meanl ~ dnorm(0,0.001)
   sdl ~ dunif(0,10)
   taul <- pow(sdl,-2)
   
   for(i in 1:nspecies){
+    for(k in 1:ntrapdays){
+      betad[i,k] ~ dnorm(0,1)
+    }
     for(m in 1:2){
-        a[i,m] ~ dnorm(meand,1/sdd)
-        logit(d[i,m]) <- a[i,m]
+        #a[i,m] ~ dnorm(meand,1/sdd)
+        #logit(d[i,m]) <- a[i,m]
+        alpha[i,m] ~ dnorm(meand,taud)
         b[i,m] ~ dnorm(meanl,taul)
         log(lambda[i, m]) <- b[i,m]
     }
@@ -259,13 +403,13 @@ cat(paste(model1.string))
 sink()
 
 DBmod_OD <- rjags::jags.model(file = "JAGS\\Nmod_overdisp.txt",
-                                data=list(count = dbtab, hab=hab, nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
+                                data=list(count = dbtab, hab=hab,rebait=c(1,2,1,2), nspecies= nspecies, nsites = nsites, ntrapdays = ntrapdays),
                                 inits =list(abun=dbtab),
-                                n.chains=3,n.adapt=1000)
-update(object = DBmod_OD, n.iter = 1000)
+                                n.chains=3,n.adapt=10000)
+update(object = DBmod_OD, n.iter = 10000)
 DBmod_OD <- rjags::coda.samples(model = DBmod_OD,
                                  variable.names = c("d","lambda","meand","sdd"),
-                                 n.iter=1000, thin = 1)
+                                 n.iter=10000, thin = 1)
 
 MCMCvis::MCMCtrace(DBmod_OD,wd="C:\\Users\\Jorgen\\Desktop",filename="DBmod_OD_trace.pdf")
 
